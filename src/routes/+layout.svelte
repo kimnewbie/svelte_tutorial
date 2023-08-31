b263da8e9ac2ed4df1656b9559a739bbc323c5cf
youjin kim <gracecancho@gmail.com>
2023-08-31 15:52:11 +0900

    part 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,65 +1,9 @@
-<nav>
-    <div class="container">
-        <h1>YJ</h1>
-        <ul>
-            <li>
-                <a href="/">HOME</a>
-            </li>
-            <li>
-                <a href="/about">About</a>
-            </li>
-            <li>
-                <a href="/services">Services</a>
-            </li>
-            <li>
-                <a href="/contact">Contact</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+<script>
+import Navbar from "./Navbar.svelte";
+</script>
 
-<slot />
+<Navbar />
 
-<style>
-nav {
-    padding: 0.5em;
-    background-color: #343a40;
-    color: white;
-}
-
-.container {
-    display: flex;
-    align-items: center;
-}
-
-ul {
-    display: flex;
-    margin: 0;
-    margin-left: auto;
-    list-style: none;
-    font-size: 1em;
-}
-
-li {
-    margin-right: 20px;
-}
-
-h1 {
-    margin: 0;
-    font-size: 1.3em;
-    font-weight: normal;
-}
-
-a {
-    text-decoration: none;
-    color: #aaa;
-}
-
-.logo {
-    color: white;
-}
-
-.active {
-    color: white;
-}
-</style>
+<div class="container">
+    <slot />
+</div>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,70 @@
+<script>
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+  $: routeId = $page.route.id;
+//   $: console.log($page); // 페이지 변경될 때마다 라우팅
+</script>
+<nav>
+    <div class="container">
+        <!-- <a href='/'><h1>YJ</h1></a>  -->
+         <h1>YJ</h1> 
+        <ul>
+            <li>
+                <a href="/" class:active={routeId == '/'}>HOME</a>
+            </li>
+            <li>
+                <a href="/about" class:active={routeId == '/about'}>About</a>
+            </li>
+            <li>
+                <a href="/services" class:active={routeId == '/services'}>Services</a>
+            </li>
+            <li>
+                <a href="/contact" class:active={routeId == '/contact'}>Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav> 
+
+<style>
+nav {
+    padding: 0.5em;
+    background-color: #343a40;
+    color: white;
+}
+
+.container {
+    display: flex;
+    align-items: center;
+}
+
+ul {
+    display: flex;
+    margin: 0;
+    margin-left: auto;
+    list-style: none;
+    font-size: 1em;
+}
+
+li {
+    margin-right: 20px;
+}
+
+h1 {
+    margin: 0;
+    font-size: 1.3em;
+    font-weight: normal;
+}
+
+a {
+    text-decoration: none;
+    color: #aaa;
+} 
+
+.logo {
+    color: white;
+}
+
+.active {
+    color: white;
+}
+</style>
